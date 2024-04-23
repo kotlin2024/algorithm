b311e007fd30a0c528d3d3b2f5c9e928e373dd07
HJP_Kotlin_2 <45454119+kotlin2024@users.noreply.github.com>
2024-04-23 10:47:52 +0900

    [level 0] Title: 각도기, Time: 0.02 ms, Memory: 60.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120829. 각도기/각도기.kt
@@ -0,0 +1,18 @@
+class Solution {
+    fun solution(angle: Int): Int {
+        var answer: Int
+        if(angle==180){
+            answer=4
+        }
+        else if(angle >90){
+            answer=3
+        }
+        else if(angle ==90){
+            answer=2
+        }
+        else{
+            answer=1
+        }
+        return answer
+    }
+}

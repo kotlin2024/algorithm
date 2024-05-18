862d71e4a1ae8b01b713c38893114af9ae7cf9a1
HJP_Kotlin_2 <45454119+kotlin2024@users.noreply.github.com>
2024-05-18 10:43:58 +0900

    [level 1] Title: 과일 장수, Time: 16.77 ms, Memory: 63.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.kt
@@ -0,0 +1,13 @@
+class Solution {
+    fun solution(k: Int, m: Int, score: IntArray): Int {
+    var answer: Int = 0
+    score.sortDescending()
+
+    for (i in m-1 until score.size step m) {
+        answer += score[i] * m
+    }
+
+    return answer
+}
+
+}
